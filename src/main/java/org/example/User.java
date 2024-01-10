f55709be52458776be40630c687707a59894957f
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-10 21:10:57 +0200

    class Product

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    private Integer id;
+    private String username;
+    private Map<Product, Integer> cart;
+
+    public User(Integer id, String username) {
+        this.id = id;
+        this.username = username;
+        this.cart = new HashMap<>();
+    }
+
+
+    public void addToCart(Product product, int quantity) {
+        cart.put(product, cart.getOrDefault(product, 0) + quantity);
+    }
+
+    public void removeFromCart(Product product, int quantity) {
+        if (cart.containsKey(product)) {
+            int currentQuantity = cart.get(product);
+            if (currentQuantity - quantity <= 0) {
+                cart.remove(product);
+            } else {
+                cart.put(product, currentQuantity - quantity);
+            }
+        }
+    }
+
+    public void modifyCart(Product product, int newQuantity) {
+        if (cart.containsKey(product) && newQuantity > 0) {
+            cart.put(product, newQuantity);
+        }
+    }
+}
